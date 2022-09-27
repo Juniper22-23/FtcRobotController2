6a880f7948e06662f4749e76ccda2a4987ef772d
school17729 <school17729@gmail.com>
2022-09-27 17:23:36 -0400

    Testing GitHub

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFolder/AutonomousClass.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.AutonomousFolder;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
+public class AutonomousClass extends LinearOpMode {
+
+    // declare class variables here
+
+    public void runOpMode() {
+
+        // setup
+
+        while (opModeIsActive()) {
+            // main loop
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverContraul/DriverControlClass.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.DriverContraul;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class DriverControlClass extends LinearOpMode {
+
+    // declare class variables here
+
+    public void runOpMode() {
+
+        // setup
+
+        while (opModeIsActive()) {
+            // main loop
+        }
+    }
+}
