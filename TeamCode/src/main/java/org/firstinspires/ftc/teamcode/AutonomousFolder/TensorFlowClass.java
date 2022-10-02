b62592a83bf6166dab632f37991afdc0ce9e73d6
school17729 <school17729@gmail.com>
2022-10-02 15:58:08 -0400

    Yeet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFolder/TensorFlowClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFolder/TensorFlowClass.java
@@ -19,7 +19,7 @@ public class TensorFlowClass extends AutomatorClass {
     private VuforiaLocalizer vuforia;
 
     private static final String modelTerry = "<tflite file>";
-    private static final String[] labelTerry = {"Ball", "Cube", "Duck", "Marker"};
+    private static final String[] labelTerry = {"Bal", "Cube", "Duck", "Marker"};
 
     public ArrayList<String> recognitionLabels;
     public double terryDuckLeft;
