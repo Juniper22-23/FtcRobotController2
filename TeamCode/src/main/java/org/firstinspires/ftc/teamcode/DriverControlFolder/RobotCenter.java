4fcce6e6c935784756bc8b74ef8e725b32f22f77
school17729 <school17729@gmail.com>
2022-10-08 15:48:25 -0400

    RobotCenter stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlFolder/RobotCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlFolder/RobotCenter.java
@@ -13,89 +13,34 @@ public class RobotCenter extends Drivetrain {
     }
 
     public void drive(double gamepadX, double gamepadY, double gamepadRot) {
-        double rotationEffectiveness;
-        double xyEffectiveness;
+        double rotationEffectiveness = 0.50;
+        double xyEffectiveness = 0.75;
 
-        double normalRot = gamepad1.right_stick_x;
-        double normalX = gamepad1.left_stick_x;
-        double normalY = gamepad1.left_stick_y;
+        double turn = gamepadRot * rotationEffectiveness;
+        double x = gamepadX * xyEffectiveness;
+        double y = gamepadY * xyEffectiveness;
 
-        double theta = Math.atan2(normalY,normalX);
-        double power = Math.hypot(normalX,normalY);
+        double theta = Math.atan2(y,x);
+        double power = Math.hypot(x,y);
         double sin = Math.sin(theta - Math.PI/4);
         double cos = Math.cos(theta - Math.PI/4);
         double max = Math.max(Math.abs(sin),Math.abs(cos));
 
-        double leftBackPower;
-        double leftFrontPower;
-        double rightBackPower;
-        double rightFrontPower;
+        double leftBackPower = power * cos/max + turn;
+        double leftFrontPower = power * sin/max - turn;
+        double rightBackPower = power * sin/max + turn;
+        double rightFrontPower = power * cos/max + turn;
 
-        rotationEffectiveness = 0.50;
-        xyEffectiveness = 0.75;
-
-        normalRot = gamepadRot * rotationEffectiveness;
-        normalX = gamepadX * xyEffectiveness;
-        normalY = gamepadY * xyEffectiveness;
-
-
-        leftBackPower = power * cos/max + normalRot;
-        leftFrontPower = power * sin/max - normalRot;
-        rightBackPower = power * sin/max + normalRot;
-        rightFrontPower = power * cos/max + normalRot;
-
-        leftBackMotor.setPower(leftBackPower);
-        leftFrontMotor.setPower(leftFrontPower);
-        rightBackMotor.setPower(rightBackPower);
-        rightFrontMotor.setPower(rightFrontPower);
-    }
-/*
-    public void revolve(double magnitude) {
-        // in inches
-        double robotLength = 13.22; // distance between axles
-        double spaceFromShipHub = 1.0;
-        double shipHubRadius = 9.0;
-        //double revolveFactor = ( (shipHubRadius + spaceFromShipHub) / (robotLength + spaceFromShipHub + shipHubRadius) );
-        double revolveFactor = 0.3;
-
-        double sign = magnitude / Math.abs(magnitude);
-
-        double leftBackPower;
-        double leftFrontPower;
-        double rightBackPower;
-        double rightFrontPower;
-
-        double leftBackBias = 0.3;
-        double leftFrontBias = 0.3;
-        double rightBackBias = 0.3;
-        double rightFrontBias = 0.3;
-
-        if (magnitude == 0) {
-            leftBackPower = -(magnitude + leftBackBias - leftBackBias);
-            rightBackPower = magnitude;
-            rightFrontPower = -(magnitude * revolveFactor);
-        } else if (magnitude == -1) {
-            leftBackPower = -(magnitude - leftBackBias);
-            rightBackPower = magnitude + rightBackBias;
-            rightFrontPower = -(magnitude * revolveFactor) - rightFrontBias;
-        } else if (magnitude == 1) {
-            leftBackPower = -(magnitude - leftBackBias);
-            rightBackPower = magnitude - rightBackBias;
-            rightFrontPower = -(magnitude * revolveFactor) - rightFrontBias;
-        } else {
-            leftBackPower = -magnitude;
-            leftFrontPower = magnitude * revolveFactor;
-            rightBackPower = magnitude;
-            rightFrontPower = -magnitude * revolveFactor;
+        if ((power + Math.abs(turn)) > 1) {
+            leftFrontPower /= power + turn;
+            rightFrontPower /= power + turn;
+            leftBackPower /= power + turn;
+            rightBackPower /= power + turn;
         }
-        leftFrontPower = (magnitude * revolveFactor) + magnitude * leftFrontBias;
 
         leftBackMotor.setPower(leftBackPower);
         leftFrontMotor.setPower(leftFrontPower);
         rightBackMotor.setPower(rightBackPower);
         rightFrontMotor.setPower(rightFrontPower);
-
     }
-
- */
 }
