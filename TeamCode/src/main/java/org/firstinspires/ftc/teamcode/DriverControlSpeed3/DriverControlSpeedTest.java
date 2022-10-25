6443971e6d6c484583ea37b41d8a48c1648281e8
juniperrobotics <71951482+juniperrobotics@users.noreply.github.com>
2022-10-25 16:28:34 -0400

    lol life part 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlSpeed3/DriverControlSpeedTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.DriverControlSpeed3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+class DriverControlSpeedTest {
+    @Test
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlSpeed3/simpleTest.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.DriverControlSpeed3;
+
+public class simpleTest {
+    public int add(int numberA, int numberB){
+        return numberA + numberB;
+    }
+}
