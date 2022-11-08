f1552bfcc23e8bcd6c4014faa8dcdae0e1d7e077
juniperrobotics <71951482+juniperrobotics@users.noreply.github.com>
2022-11-08 15:29:30 -0500

    Normal Drive = Field Centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlFolder/DriverControlClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlFolder/DriverControlClass.java
@@ -11,7 +11,7 @@ import org.firstinspires.ftc.teamcode.RobotCenter;
 // Robot center programming
 // tensor https://www.youtube.com/watch?v=2FmcHiLCwTU help
 
-@TeleOp(name = "MainDrive", group = "Tele-Op")
+@TeleOp(name = "DriverControlClass", group = "Tele-Op")
 public class DriverControlClass extends LinearOpMode {
 
     // declare class variables here
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlSpeed3/aa.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlSpeed3/aa.java
@@ -1,70 +1,69 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.DriverControlSpeed3;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-public class FieldCenter extends Drivetrain {
-    public FieldCenter(Telemetry telemetry, HardwareMap hardwareMap) {
+import org.firstinspires.ftc.teamcode.Drivetrain;
+import org.firstinspires.ftc.teamcode.Mathematics;
+
+public class aa extends Drivetrain {
+    private Orientation angles;
+
+    //This is the tweaked version of Field Center.
+    public aa(Telemetry telemetry, HardwareMap hardwareMap) {
         super(telemetry, hardwareMap);
+
     }
 
     @Override
     public void drive(double gamepadX, double gamepadY, double gamepadRot) {
-        telemetry.addData("gamepadX: ", gamepadX);
-        telemetry.addData("gamepadY: ", gamepadY);
-        telemetry.addData("gamepadRot: ", gamepadRot);
+        double rotationEffectiveness = 1;
+        double xyEffectiveness = 1;
 
-        double rotationEffectiveness = 1.0;
-        telemetry.addData("rotationEffectiveness: ", rotationEffectiveness);
-        double xyEffectiveness = 1.0;
-        telemetry.addData("xyEffectiveness: ", xyEffectiveness);
+
+        angles = new Orientation();
 
         // gamepadRot is negated because in math, a counterclockwise rotation is positive
         // (think unit circle), but on the controller, we expect the robot to rotate clockwise when
         // we push the stick to the right. Pushing the stick to the right outputs a positive value.
         double turn = -gamepadRot * rotationEffectiveness;
-        telemetry.addData("turn: ", turn);
         double controllerX = gamepadX * xyEffectiveness;
-        telemetry.addData("controllerX: ", controllerX);
         double controllerY = gamepadY * xyEffectiveness;
-        telemetry.addData("controllerY: ", controllerY);
         double[] controllerVector = {controllerX, controllerY};
-        telemetry.addData("controllerVector[0]: ", controllerVector[0]);
-        telemetry.addData("controllerVector[1]: ", controllerVector[1]);
+
 
         double imuMeasure = readFromIMU();
-        telemetry.addData("imuMeasure: ", imuMeasure);
 
         double[] rotatedVector = Mathematics.rotate(controllerVector, imuMeasure);
-        telemetry.addData("rotatedVector[0]: ", rotatedVector[0]);
-        telemetry.addData("rotatedVector[1]: ", rotatedVector[1]);
         double rotatedX = rotatedVector[0];
         double rotatedY = rotatedVector[1];
-        telemetry.addData("rotatedX: ", rotatedX);
-        telemetry.addData("rotatedY: ", rotatedY);
 
         double theta = Math.atan2(rotatedY, rotatedX);
-        telemetry.addData("theta: ", theta);
         double power = Math.hypot(rotatedX, rotatedY);
-        telemetry.addData("power: ", power);
         double sin = Math.sin(theta - Math.PI/4);
-        telemetry.addData("sin: ", sin);
         double cos = Math.cos(theta - Math.PI/4);
-        telemetry.addData("cos: ", cos);
-
         double max = Math.max(Math.abs(sin),Math.abs(cos));
-        telemetry.addData("max: ", max);
 
         double leftBackPower = power * sin/max + turn;
         double leftFrontPower = power * cos/max + turn;
         double rightBackPower = power * cos/max - turn;
         double rightFrontPower = power * sin/max - turn;
-        telemetry.addLine("before capping: ");
-        telemetry.addData("leftBackPower: ", leftBackPower);
-        telemetry.addData("leftFrontPower: ", leftFrontPower);
-        telemetry.addData("rightBackPower: ", rightBackPower);
-        telemetry.addData("rightFrontPower: ", rightFrontPower);
+
+        double pi = 3.1415926;
+
+        double gyro_degrees = ahrs->angles;
+        double gyro_radians = gyro_degrees * pi/180;
+
+        double temp = controllerY * Math.cos(gyro_radians) +
+                controllerX * Math.sin(gyro_radians);
+        controllerX = -controllerY * Math.sin(gyro_radians) +
+                controllerX * Math.cos(gyro_radians);
+        controllerY = temp;
 
         if ((power + Math.abs(turn)) > 1) {
             leftFrontPower /= power + turn;
@@ -73,15 +72,22 @@ public class FieldCenter extends Drivetrain {
             rightBackPower /= power + turn;
         }
 
-        telemetry.addLine("after capping: ");
+        leftBackMotor.setPower(leftBackPower);
+        leftFrontMotor.setPower(leftFrontPower);
+        rightBackMotor.setPower(rightBackPower);
+        rightFrontMotor.setPower(rightFrontPower);
+
+        telemetry.addLine("MOTOR POWERS Method 2 Accuracy:");
+        telemetry.addData("imuMeasure: ", imuMeasure);
         telemetry.addData("leftBackPower: ", leftBackPower);
         telemetry.addData("leftFrontPower: ", leftFrontPower);
         telemetry.addData("rightBackPower: ", rightBackPower);
         telemetry.addData("rightFrontPower: ", rightFrontPower);
 
-        leftBackMotor.setPower(leftBackPower);
-        leftFrontMotor.setPower(leftFrontPower);
-        rightBackMotor.setPower(rightBackPower);
-        rightFrontMotor.setPower(rightFrontPower);
+        angles = imu.getAngularOrientation (AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        telemetry.addData(  "Heading Y-Axis", angles.firstAngle);
+        telemetry.addData(  "Roll X-Axis", angles.secondAngle);
+        telemetry.addData(  "Pitch Z-Axis", angles.thirdAngle); telemetry.update();
+
     }
 }
